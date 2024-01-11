9aca30281116081638c3861e086fcb464fb60751
newman1212 <emmanuelseworh@gmail.com>
2024-01-11 22:53:02 +0000

    making changes to form

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -18,7 +18,7 @@ const ImageLinkForm = ({onInputChange,onButtonSubmit,enterOption,visible}) => {
 		 </p>
 		 <div className ='center'>
 		 	<div className='form center pa4 br3 shadow-3' onKeyDown ={enterOption}>
-				 <input className ='f4 pa2 w-70 center' type ='tex' placeholder ='enter image url here' onChange ={onInputChange}/>
+				 <input className ='f4 pa2 w-70 center' type ='text' placeholder ='enter image url here' onChange ={onInputChange}/>
 				 <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
 				 onClick ={onButtonSubmit}> 
 				 {visible?(<span className='center'><Spinner radius={20} color= "white" stroke={3} visible={visible} /></span>) 
